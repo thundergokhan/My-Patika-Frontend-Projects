2faa1d00b0d7f7a36f0f10fd50ff6250714ec0f8
Gökhan Yıldırım <82091624+thundergokhan@users.noreply.github.com>
2022-09-15 17:52:47 +0300

    Update 11-prompt-ile-kullanicidan-bilgi-almak.js

--- JavaScript-Kategorisi/js/11-prompt-ile-kullanicidan-bilgi-almak.js
+++ JavaScript-Kategorisi/js/11-prompt-ile-kullanicidan-bilgi-almak.js
@@ -1,8 +1,12 @@
 // ********** prompt ile Kullanicidan Bilgi Almak **********
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
 
-let fullName = prompt("Lutfen Adinizi Giriniz: ")
+let fullName = prompt("Lutfen Adinizi Giriniz: ");
 
-let greeting = document.querySelector("#greeting")
+let greeting = document.querySelector("#greeting");
 
-greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`
+greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`;
+
+// Javascript kullanıcı ile iletişim kurmamızı sağlayabilen etkileşimli
+//  bir dildir ve biliyoruz ki kullanıcı ile etkileşimi
+//  sağlamının bir yolu kullanıcıdan bilgi istemektir.
